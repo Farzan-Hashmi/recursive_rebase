da7d3239b74236fa1956a93ef5fa7dedda64feda
Farzan Hashmi <farzan.ahmad.hashmi@gmail.com>
2024-08-30 16:21:52 -0700

    msg

--- recursive_rebase/main.py
+++ recursive_rebase/main.py
@@ -1,6 +1,7 @@
 import argparse
 from collections import defaultdict
 import subprocess
+import json
 
 # branch name format: 'stack_tag/branch_info/number'
 
@@ -24,14 +25,39 @@ def get_sorted_branches_in_stack(stack_tag):
         .split("\n")
     )
     branches = [branch.strip() for branch in output if branch]
-    print(f"Branches: {branches}")
     branches = [branch[1:] if branch[0] == "*" else branch for branch in branches]
     branches = [branch.strip() for branch in branches]
-
-    print(f"Branches: {branches}")
-
     branches.sort(key=lambda x: int(x.split("/")[-1]))
     return branches
+
+
+def print_pr_stack(current_branch):
+    branches = get_sorted_branches_in_stack(current_branch.split("/")[0])
+    stack = []
+    for i in range(len(branches)):
+        branch = branches[i]
+        pr_title = subprocess.check_output(
+            f"gh pr view {branch} --json title", shell=True
+        ).decode("utf-8")
+        pr_title_dict = json.loads(pr_title)
+        pr_link = subprocess.check_output(
+            f"gh pr view {branch} --json url", shell=True
+        ).decode("utf-8")
+        pr_link_dict = json.loads(pr_link)
+        title = pr_title_dict["title"]
+        link = pr_link_dict["url"]
+        append_str = f"[{title}]({link})"
+        if branch == current_branch:
+            append_str += " <- here"
+        append_str += "\n"
+        stack.append(append_str)
+
+    stack.reverse()
+
+    stack_str = "".join(stack)
+
+    print("Stack:")
+    print(stack_str)
 
 
 def sync(stack_tag, start_number):
@@ -41,9 +67,22 @@ def sync(stack_tag, start_number):
     for i in range(start_number + 1, len(branches)):
         if i == 0:
             rebase_onto("main", branches[i])
+            force_push(branches[i])
+            print(f"PR base for {branches[i]} to main")
+            subprocess.check_output(
+                f"gh pr edit {branches[i]} --base main", shell=True
+            ).decode("utf-8")
+            print(f"Printing PR body for {branches[i]}")
+            print_pr_stack(branches[i])
         else:
             rebase_onto(branches[i - 1], branches[i])
             force_push(branches[i])
+            print(f"PR base for {branches[i]} to {branches[i-1]}")
+            subprocess.check_output(
+                f"gh pr edit {branches[i]} --base {branches[i-1]}", shell=True
+            ).decode("utf-8")
+            print(f"Printing PR body for {branches[i]}")
+            print_pr_stack(branches[i])
 
 
 def main():
