cada982a252779fd7cfc10b37f7f9ef9ee4db9f5
Farzan Hashmi <farzan.ahmad.hashmi@gmail.com>
2024-08-30 01:51:18 -0700

    wip

--- recursive_rebase/main.py
+++ recursive_rebase/main.py
@@ -1,14 +1,50 @@
 import argparse
+from collections import defaultdict
+import subprocess
 
+# branch name format: 'stack_tag/branch_info/number'
+
+
+def rebase_onto(branch_to_rebase_on, branch_to_rebase):
+    print(f"Rebasing {branch_to_rebase} onto {branch_to_rebase_on}")
+    output = subprocess.check_output(
+        f"git rebase {branch_to_rebase_on} {branch_to_rebase}", shell=True
+    ).decode("utf-8")
+
+
+def force_push(branch):
+    print(f"Force pushing {branch}")
+    output = subprocess.check_output(f"git push -f origin {branch}", shell=True).decode(
+        "utf-8"
+    )
+
+
+def get_sorted_branches_in_stack(stack_tag):
+    output = (
+        subprocess.check_output(f"git branch --list {stack_tag}/*", shell=True)
+        .decode("utf-8")
+        .split("\n")
+    )
+    branches = [branch.split()[0] for branch in output if branch]
+    branches.sort(key=lambda x: int(x.split("/")[-1]))
+    return branches
+
+
+def sync(stack_tag):
+    branches = get_sorted_branches_in_stack(stack_tag)
+    for i in range(1, len(branches)):
+        rebase_onto(branches[i - 1], branches[i])
+        force_push(branches[i])
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Recursively rebase a series of common branches to create a stack of branches.')
-    parser.add_argument('branch', help='The branch to rebase from')
+    parser = argparse.ArgumentParser(
+        description="Recursively rebase a series of common branches to create a stack of branches."
+    )
+    parser.add_argument("stack_tag", help="The common tag for the stack of branches.")
     args = parser.parse_args()
-
-    #TODO: Implement the rebase logic
-    print(f"Rebasing {args.branch}")
+    sync(args.stack_tag)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    main()
