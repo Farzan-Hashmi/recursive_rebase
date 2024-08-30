a84162b55c7667f6dd59138dbbfbae94ee09ba7e
Farzan Hashmi <farzan.ahmad.hashmi@gmail.com>
2024-08-30 14:44:10 -0700

    wip

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='recursive_rebase',
-    version='0.1',
+    name="recursive_rebase",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires=[],
     entry_points={
-        'console_scripts': [
-            'recursive-rebase = recursive_rebase.main:main',
+        "console_scripts": [
+            "recursive-rebase = recursive_rebase.main:main",
         ],
     },
 )
